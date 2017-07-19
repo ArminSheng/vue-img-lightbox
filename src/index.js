c5136d030b51355fffd84bfe80b68c9d5756e500
arminsheng <zhimin.sh@cn.micropoplar.com>
2017-07-19 19:16:16 +0800

    update bind and update hook

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
 import ImgFull from './ImgFull'
 
+let Lightbox
 let instance
+
+const initialize = (el, binding) => {
+  const src = binding.value || el.src
+
+  if (!src) {
+    // console.error('Binding element missing src!')
+    return
+  }
+
+  el.onclick = () => {
+    if (!instance) {
+      const container = document.createElement('div')
+      container.id = 'v-img-view'
+
+      document.body.appendChild(container)
+      instance = new Lightbox().$mount('#v-img-view')
+    }
+
+    instance.src = src
+    instance.open()
+  }
+}
 
 // Vue install
 const install = (Vue) => {
-  const Lightbox = Vue.extend(ImgFull)
+  Lightbox = Vue.extend(ImgFull)
 
   Vue.directive('img-view', {
-    bind (el, binding) {
-      const src = binding.value || el.src
-
-      if (!src) {
-        console.error('Binding element missing src!')
-        return
-      }
-
-      el.onclick = () => {
-        if (!instance) {
-          const container = document.createElement('div')
-          container.id = 'v-img-view'
-
-          document.body.appendChild(container)
-          instance = new Lightbox().$mount('#v-img-view')
-        }
-
-        instance.src = src
-        instance.open()
-      }
-    }
+    bind: initialize,
+    update: initialize
   })
 }
 
